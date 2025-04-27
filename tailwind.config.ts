6d6e5335692273f59f1362c4b2b29f1970392fe6
Gursimran Singh <sgursimranmatharu@gmail.com>
2025-04-28 01:29:34 +0530

    feat: add custom font files and update Tailwind CSS configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,5 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 module.exports = {
     darkMode: 'class',
     theme: {
@@ -6,6 +8,16 @@ module.exports = {
                 background: 'var(--background)',
                 foreground: 'var(--foreground)',
             },
+            fontFamily: {
+                sans: ['var(--font-urbane)', ...fontFamily.sans],
+                urbane: ['Urbane', ...fontFamily.sans],
+                productsans: ['var(--font-productsans)', ...fontFamily.sans],
+            },
         },
-    }
+    },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ]
 }
